762dc94635645e9add3aad20c437720c77fc23f8
luke <lukehoang195@gmail.com>
2022-03-31 15:12:23 +0700

    pagekage

--- src/sections/package.js
+++ src/sections/package.js
@@ -241,7 +241,20 @@ const responsive = {
 
 export default function Package() {
   const { monthly, annual } = packages;
-
+  const [state, setState] = useState({
+    active: "monthly",
+    pricingPlan: monthly,
+  });
+  const handlePricing = (plan) => {
+    if (plan === "annual") {
+      setState({ active: "annual", pricingPlan: annual });
+    } else {
+      setState({
+        active: "monthly",
+        pricingPlan: monthly,
+      });
+    }
+  };
   const sliderParams = {
     additionalTransfrom: 0,
     arrows: false,
@@ -265,7 +278,42 @@ export default function Package() {
     sliderClass: "",
   };
 
-  return <h1>Package</h1>;
+  return (
+    <section id="pricing" sx={{ variant: `section.pricing` }}>
+      <Container>
+        <SectionHeader slogan="Pricing Plan" title="Choose your pricing plan" />
+        <Flex sx={styles.buttonGroup}>
+          <Box sx={styles.buttonGroupInner}>
+            <button
+              className={state.active === "monthly" ? "active" : ""}
+              type="button"
+              onClick={() => handlePricing("monthly")}
+              aria-label="monthly"
+            >
+              Monthly Plan
+            </button>
+            <button
+              className={state.active === "annual" ? "active" : ""}
+              type="button"
+              onClick={() => handlePricing("annual")}
+              aria-label="annual"
+            >
+              Annual Plan
+            </button>
+          </Box>
+        </Flex>
+        <Box sx={styles.pricingWrapper} className="pricing__wrapper">
+          <Carousel {...sliderParams}>
+            {state.pricingPlan.map((packageData) => (
+              <Box sx={styles.pricingItem} key={packageData.id}>
+                <PriceCard data={packageData} />
+              </Box>
+            ))}
+          </Carousel>
+        </Box>
+      </Container>
+    </section>
+  );
 }
 
 const fadeIn = keyframes`
